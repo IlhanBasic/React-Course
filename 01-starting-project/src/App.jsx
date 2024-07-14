0b032e8d7672b33254451ce5f23c5c60e1d12e86
Ilhan Basic <ilhanbasic456@gmail.com>
2024-07-14 15:57:32 +0200

    added active className on buttons

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {useState} from 'react';
 import Header from './components/Header/Header.jsx';
 import {CORE_CONCEPTS} from './data.js';
+import {EXAMPLES} from './data-with-examples.js';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton/TabButton.jsx'
 
 function App(){
-  const [Counter,setCounter] = useState('Please click a button');
+  const [Counter,setCounter] = useState();
  function handleSelect(clickedButton){
     setCounter(clickedButton);
  }
@@ -26,12 +27,21 @@ function App(){
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={()=>handleSelect('Components')}>Components</TabButton>
-            <TabButton onSelect={()=>handleSelect('JSX')}>JSX</TabButton>
-            <TabButton onSelect={()=>handleSelect('Props')}>Props</TabButton>
-            <TabButton onSelect={()=>handleSelect('States')}>States</TabButton>
+            <TabButton isSelect={Counter === 'components'} onSelect={()=>handleSelect('components')}>Components</TabButton>
+            <TabButton isSelect={Counter === 'jsx'} onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelect={Counter === 'props'} onSelect={()=>handleSelect('props')}>Props</TabButton>
+            <TabButton isSelect={Counter === 'state'} onSelect={()=>handleSelect('state')}>States</TabButton>
           </menu>
-          {Counter}
+          {!Counter && <p>Please click a button</p>}
+          {Counter && <div id='tab-content'>
+            <h3>{EXAMPLES[Counter].title}</h3>
+            <p>{EXAMPLES[Counter].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[Counter].code}
+              </code>
+            </pre>
+          </div>}
         </section>
 
       </main>
--- 01-starting-project/src/components/TabButton/TabButton.jsx
+++ 01-starting-project/src/components/TabButton/TabButton.jsx
@@ -1,4 +1,4 @@
-export default function TabButton({children,onSelect}){
-    return <li><button onClick={onSelect}>{children}</button></li>
+export default function TabButton({children,onSelect,isSelect}){
+    return <li><button className={isSelect?'active':null} onClick={onSelect}>{children}</button></li>
     //mozes i samo children ako radis preko drestrukturisanja
 }
