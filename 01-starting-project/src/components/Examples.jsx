a00c932635931b5018e2acde545484110121f0de
Ilhan Basic <ilhanbasic456@gmail.com>
2024-07-15 13:30:05 +0200

    changed dinamically type components

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -2,30 +2,38 @@ import {useState} from 'react';
 import {EXAMPLES} from '../data-with-examples.js';
 import TabButton from './TabButton/TabButton.jsx';
 import Section from './Section.jsx'
-
+import Tabs from './Tabs.jsx';
 export default function Examples (){
     const [Counter,setCounter] = useState();
+    let tabContent = <p>Please click a button</p>;
+    if (Counter) {
+      tabContent = (
+        <div id='tab-content'>
+          <h3>{EXAMPLES[Counter].title}</h3>
+          <p>{EXAMPLES[Counter].description}</p>
+          <pre>
+            <code>
+              {EXAMPLES[Counter].code}
+            </code>
+          </pre>
+        </div>
+      );
+    }
     function handleSelect(clickedButton){
         setCounter(clickedButton);
      }
     return (
         <Section title="Examples" id='examples'>
-          <menu>
+          <Tabs ButtonsContainer='menu' buttons={
+            <>
             <TabButton isSelect={Counter === 'components'} onClick={()=>handleSelect('components')}>Components</TabButton>
             <TabButton isSelect={Counter === 'jsx'} onClick={()=>handleSelect('jsx')}>JSX</TabButton>
             <TabButton isSelect={Counter === 'props'} onClick={()=>handleSelect('props')}>Props</TabButton>
             <TabButton isSelect={Counter === 'state'} onClick={()=>handleSelect('state')}>States</TabButton>
-          </menu>
-          {!Counter && <p>Please click a button</p>}
-          {Counter && <div id='tab-content'>
-            <h3>{EXAMPLES[Counter].title}</h3>
-            <p>{EXAMPLES[Counter].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[Counter].code}
-              </code>
-            </pre>
-          </div>}
+            </>}>
+            {tabContent}
+          </Tabs>
+          
         </Section>
     )
 }
