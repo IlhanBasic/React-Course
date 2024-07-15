d037ce6b6e5d59a92f483b27a5ec96ff974f4beb
Ilhan Basic <ilhanbasic456@gmail.com>
2024-07-15 19:00:23 +0200

    enabled players name in input

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,8 +1,27 @@
-export default function Player({name,symbol}){
+import { useState } from "react";
+export default function Player({initialName,symbol}){
+    const [playerName,setPlayerName] = useState(initialName);
+    const [isEditing,setIsEditing] = useState(false);
+    //funkcija za azuriranje UI
+    function handleEditClick(){
+        setIsEditing(!isEditing);
+    }
+    function handleChange(event){
+        setPlayerName(event.target.value);
+    }
+    
+    let editablePlayerName =  <span className="player-name">{playerName}</span>;
+    //prikaz inputa u zavisnosti od isEditing
+    if(isEditing){
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
+    }
     return (
         <li>
-            <span className="player-name">{name}</span>
-            <span className="player-symbol">{symbol}</span>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>  
         </li>
     )
 }
