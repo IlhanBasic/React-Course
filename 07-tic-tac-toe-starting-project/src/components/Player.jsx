ac0db0ecbc612799d328b2d38175b105157754cc
Ilhan Basic <ilhanbasic456@gmail.com>
2024-07-16 09:54:15 +0200

    added GameBoard.jsx and some click logic

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-export default function Player({initialName,symbol}){
+export default function Player({initialName,symbol,isActive}){
     const [playerName,setPlayerName] = useState(initialName);
     const [isEditing,setIsEditing] = useState(false);
     //funkcija za azuriranje UI
     function handleEditClick(){
-        setIsEditing(!isEditing);
+        setIsEditing(editing=>!editing);
     }
     function handleChange(event){
         setPlayerName(event.target.value);
@@ -16,7 +16,7 @@ export default function Player({initialName,symbol}){
         editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
     }
     return (
-        <li>
+        <li className={isActive?'active':undefined}>
             <span className="player">
                 {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
