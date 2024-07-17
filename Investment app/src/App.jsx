dc4589208f20eeff99845b3e7aaa5f0a78bd1038
Ilhan Basic <ilhanbasic456@gmail.com>
2024-07-17 19:29:04 +0200

    added Results component

--- Investment app/src/App.jsx
+++ Investment app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header"
 import UserInput from "./components/UserInput"
-
+import Results from "./components/Results";
 function App() {
   const [userInput, setUserInput] = useState({
     initialInvestment: 1000,
@@ -17,10 +17,13 @@ function App() {
       }
     });
   }
+  const isValidInput = userInput.duration <= 0 && userInput.duration;
   return (
     <>
     <Header/>
-    <UserInput/>
+    <UserInput userInput={userInput} onChange={handleChange}/>
+      {isValidInput && <p className="center">Please enter value which is more than 0 and less than 100</p> }
+      {!isValidInput && <Results userInput={userInput}/>}
     </>
     
   )
