2b96297b08df591a25033bcfe6324085adb2d7c6
Ilhan Basic <ilhanbasic456@gmail.com>
2024-07-17 19:49:45 +0200

    Deleted directory

--- Investment app/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const src = '../../public/investment-calculator-logo.png';
-export default function Header (){
-    return (
-        <header id="header">
-            <img src={src}/>
-            <h1>Investment Calculator</h1>
-        </header>
-    )
-}
