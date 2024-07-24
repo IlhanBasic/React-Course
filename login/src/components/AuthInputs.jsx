e80471154d6a848cd86e61f5f4ccb656e427bd74
Ilhan Basic <ilhanbasic456@gmail.com>
2024-07-24 15:55:32 +0200

    new way with portal

--- login/src/components/AuthInputs.jsx
+++ login/src/components/AuthInputs.jsx
@@ -6,12 +6,6 @@ export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
   const [submitted, setSubmitted] = useState(false);
-  const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-  `
   
   
   function handleInputChange(identifier, value) {
@@ -30,8 +24,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className="flex flex-col gap-2 mb-6">
         <p className='paragraph'>
           <Input
             invalid={emailNotValid}
@@ -50,9 +44,9 @@ export default function AuthInputs() {
             }
           />
         </p>
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
