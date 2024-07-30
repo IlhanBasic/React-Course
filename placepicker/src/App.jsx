6c54d4d86765acdd9108240c9616f9649a1df053
Ilhan Basic <ilhanbasic456@gmail.com>
2024-07-30 13:34:33 +0200

    finished

--- placepicker/src/App.jsx
+++ placepicker/src/App.jsx
@@ -1,11 +1,13 @@
 import { useRef, useState, useEffect } from 'react';
-
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
+
+// const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+// const storedPlaces = storedIds.map(id=>AVAILABLE_PLACES.find(place=>place.id === id));
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
@@ -37,6 +39,10 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if(storedIds.indexOf(id)===-1){
+      localStorage.setItem('selectedPlaces',JSON.stringify([id,...storedIds]));
+    }
   }
 
   function handleRemovePlace() {
@@ -44,6 +50,8 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem('selectedPlaces',JSON.stringify(storedIds.filter(id=> id!== selectedPlace.current)));
   }
 
   return (
