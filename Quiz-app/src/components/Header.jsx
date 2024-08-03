0436657acda8d70f3774566460399801b9568cde
Ilhan Basic <ilhanbasic456@gmail.com>
2024-08-03 10:53:29 +0200

    added Header.jsx and Quiz.jsx components

---
+++ Quiz-app/src/components/Header.jsx
@@ -0,0 +1,7 @@
+const logoImg = '../../public/quiz-logo.png';
+export default function Header(){
+    return <header>
+        <img src={logoImg} alt="quiz logo" />
+        <h1>React Quiz</h1>
+    </header>
+}
