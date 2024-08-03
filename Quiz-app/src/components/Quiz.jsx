1d8b50e9ff021b5824d2ddd196dd42e7ae0728cc
Ilhan Basic <ilhanbasic456@gmail.com>
2024-08-03 12:40:45 +0200

    logic for mixing questions and preventing application crashes

--- Quiz-app/src/components/Quiz.jsx
+++ Quiz-app/src/components/Quiz.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 import QUESTIONS from '../questions.js';
+import quizCompleteImg from '../assets/quiz-complete.png'
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = userAnswers.length===QUESTIONS.length;
+    if(quizIsComplete){
+        return <div id="summary">
+            <img src={quizCompleteImg}/>
+            <h2>Quiz completed</h2>
+        </div>
+    }
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer]
         })
     }
+    
     return <div className="quiz">
         <div id="question">
             <p>{QUESTIONS[activeQuestionIndex].text}</p>
             <ul id="answers">
-                {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                {shuffledAnswers.map(answer => (
                     <li key={answer} className="answer">
                         <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                     </li>)
