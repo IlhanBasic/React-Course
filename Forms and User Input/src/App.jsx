5ac139571e5606bbcd31a601ce4f7ba89de811bf
Ilhan Basic <ilhanbasic456@gmail.com>
2024-08-07 09:32:57 +0200

    Login.jsx with submitted validation

--- Forms and User Input/src/App.jsx
+++ Forms and User Input/src/App.jsx
@@ -1,11 +1,11 @@
 import Header from './components/Header.jsx';
-import StateLogin from './components/StateLogin.jsx';
+import Login from './components/Login.jsx';
 function App() {
   return (
     <>
       <Header />
       <main>
-        <StateLogin />
+        <Login />
       </main>
     </>
   );
