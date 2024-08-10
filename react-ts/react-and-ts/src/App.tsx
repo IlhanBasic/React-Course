8bbf5e04aa6f682b07b40f6c93a22d4151f07147
Ilhan Basic <ilhanbasic456@gmail.com>
2024-08-10 21:11:33 +0200

    added useRef with type of it

--- react-ts/react-and-ts/src/App.tsx
+++ react-ts/react-and-ts/src/App.tsx
@@ -1,12 +1,17 @@
 import Todos from './components/Todos'
 import Todo from './models/Todo'
+import NewTodo from './components/Newtodo';
 function App() {
   const todo = [
     new Todo('Learn React'),
     new Todo('Learn TypeScript')
   ];
+  const addTodoHandler=(todoText:string)=>{
+
+  }
   return (
     <>
+    <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todo}/>
     </>
   )
---
+++ react-ts/react-and-ts/src/components/NewTodo.tsx
@@ -0,0 +1,19 @@
+import React, { useRef } from "react";
+
+const NewTodo:React.FC<{onAddTodo:(text:string)=>void}> = (props)=>{
+    const todoTextInputRef = useRef<HTMLInputElement>(null)
+    function submitHandler(event:React.FormEvent){
+        event.preventDefault();
+        const enteredText = todoTextInputRef.current!.value;
+        if(enteredText.trim().length===0){
+            return;
+        }
+        props.onAddTodo(enteredText);
+    }
+    return <form onSubmit={submitHandler}>
+        <label htmlFor="text"></label>
+        <input id="text" type="text" ref={todoTextInputRef}/>
+        <button>Add Todo</button>
+    </form>
+}
+export default NewTodo;
