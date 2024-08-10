494c7ba47d69e09e1d80ac5e390451b7c8b2d378
Ilhan Basic <ilhanbasic456@gmail.com>
2024-08-10 21:33:40 +0200

    added module.css to components

--- react-and-ts/src/components/NewTodo.tsx
+++ react-and-ts/src/components/NewTodo.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-
+import classes from './NewTodo.module.css';
 const NewTodo:React.FC<{onAddTodo:(text:string)=>void}> = (props)=>{
     const todoTextInputRef = useRef<HTMLInputElement>(null)
     function submitHandler(event:React.FormEvent){
@@ -10,7 +10,7 @@ const NewTodo:React.FC<{onAddTodo:(text:string)=>void}> = (props)=>{
         }
         props.onAddTodo(enteredText);
     }
-    return <form onSubmit={submitHandler}>
+    return <form onSubmit={submitHandler} className={classes.form}>
         <label htmlFor="text">Todo list</label>
         <input id="text" type="text" ref={todoTextInputRef}/>
         <button>Add Todo</button>
