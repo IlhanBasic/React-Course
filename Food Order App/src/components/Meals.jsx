ca07bb5dcae7bb168e46710769e4d6c727a7f015
Ilhan Basic <ilhanbasic456@gmail.com>
2024-08-15 09:47:23 +0200

    added custom hook useHttp.js and solved common errors

--- Food Order App/src/components/Meals.jsx
+++ Food Order App/src/components/Meals.jsx
@@ -1,23 +1,27 @@
-import { useState, useEffect } from "react";
-import MealItem from "./MealItem";
+import MealItem from './MealItem.jsx';
+import useHttp from '../hooks/useHttp.js';
+
+const requestConfig = {};
 
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
-  useEffect(() => {
-    async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-      if (!response.ok) {
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    }
-    fetchMeals();
-  }, []);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
+
+  if (isLoading) {
+    return <p>Fetching meals...</p>;
+  }
+
+  // if (!data) {
+  //   return <p>No meals found.</p>
+  // }
 
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => (
-        <MealItem meal={meal} key={meal.id} />
+        <MealItem key={meal.id} meal={meal} />
       ))}
     </ul>
   );
