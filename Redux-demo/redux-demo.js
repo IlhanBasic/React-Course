4ed1f587b80c9475e2277905a7d2bb8653003b0a
Ilhan Basic <ilhanbasic456@gmail.com>
2024-08-20 21:25:48 +0200

    added basic redux, dispatch functions and subscriber

---
+++ Redux-demo/redux-demo.js
@@ -0,0 +1,13 @@
+const redux = require('redux');
+const counterReducer = (state={counter:0},action)=>{
+    return {
+        counter:state.counter+1
+    }
+}
+const store = redux.createStore(counterReducer);
+const componentSubscriber = ()=>{
+    const latestState = store.getState();
+    console.log(latestState)
+}
+store.subscribe(componentSubscriber);
+store.dispatch({type:'increment'})
