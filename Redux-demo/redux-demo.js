2b83dfcf60608299f6d000b0c610f9e5eb54a419
Ilhan Basic <ilhanbasic456@gmail.com>
2024-08-20 21:32:29 +0200

    improved counterReducer function

--- Redux-demo/redux-demo.js
+++ Redux-demo/redux-demo.js
@@ -1,8 +1,16 @@
 const redux = require('redux');
 const counterReducer = (state={counter:0},action)=>{
-    return {
-        counter:state.counter+1
+    if(action.type === 'increment'){
+        return {
+            counter:state.counter+1
+        }
     }
+    if(action.type === 'decrement'){
+        return {
+            counter:state.counter-1
+        }
+    }
+    return state;
 }
 const store = redux.createStore(counterReducer);
 const componentSubscriber = ()=>{
@@ -11,3 +19,4 @@ const componentSubscriber = ()=>{
 }
 store.subscribe(componentSubscriber);
 store.dispatch({type:'increment'})
+store.dispatch({type:'decrement'})
