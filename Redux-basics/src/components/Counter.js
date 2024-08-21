99935500508158e75dc9e09743f6ee8139d2ea35
Ilhan Basic <ilhanbasic456@gmail.com>
2024-08-21 22:23:16 +0200

    changed Counter.jsx, added subscribe to store via useSelector, useDispatch hooks

--- Redux-basics/src/components/Counter.js
+++ Redux-basics/src/components/Counter.js
@@ -1,12 +1,24 @@
+import { useSelector,useDispatch } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   const toggleCounterHandler = () => {};
-
+  const dispatch = useDispatch();
+  function handleIncrement(){
+    dispatch({type:"increment"})
+  }
+  function handleDecrement(){
+    dispatch({type:"decrement"})
+  }
+  const counter = useSelector(state=>state.counter);
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div className='counter'>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
