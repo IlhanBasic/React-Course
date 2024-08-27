238c699fe4eb1924d4fc1c803116faec82244472
Ilhan Basic <ilhanbasic456@gmail.com>
2024-08-27 09:08:19 +0200

    added variants to motion.dialog to prevent too much copying

--- React Animations/src/components/Modal.jsx
+++ React Animations/src/components/Modal.jsx
@@ -1,18 +1,24 @@
-import { createPortal } from 'react-dom';
-import { motion } from 'framer-motion';
+import { createPortal } from "react-dom";
+import { motion } from "framer-motion";
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-      initial={{y:30,opacity:0}} 
-      animate={{y:0,opacity:1}}
-      exit={{y:30,opacity:0}}
-      open className="modal">
+        variants={{
+          hidden: { y: 30, opacity: 0 },
+          visible: { y: 0, opacity: 1 },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        open
+        className="modal"
+      >
         <h2>{title}</h2>
         {children}
       </motion.dialog>
     </>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 }
