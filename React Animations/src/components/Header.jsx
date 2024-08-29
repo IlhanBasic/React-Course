ca2c46c5fc113410e6594c12329d0549b0542873
Ilhan Basic <ilhanbasic456@gmail.com>
2024-08-29 09:08:45 +0200

    animated shared elements

--- React Animations/src/components/Header.jsx
+++ React Animations/src/components/Header.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import NewChallenge from "./NewChallenge.jsx";
+import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+
+import NewChallenge from './NewChallenge.jsx';
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -22,8 +23,8 @@ export default function Header() {
       <header id="main-header">
         <h1>Your Challenges</h1>
         <motion.button
-          whileHover={{ scale: 1.1,backgroundColor:'#8b11f0' }}
-          transition={{ type: "spring", stiffness: 500 }}
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: 'spring', stiffness: 500 }}
           onClick={handleStartAddNewChallenge}
           className="button"
         >
