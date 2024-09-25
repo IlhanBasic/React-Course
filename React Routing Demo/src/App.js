4f0b62e48f90bc84f28461be7ebf4d6395e07f40
Ilhan Basic <ilhanbasic456@gmail.com>
2024-09-25 10:42:36 +0200

    added secound route

--- React Routing Demo/src/App.js
+++ React Routing Demo/src/App.js
@@ -1,7 +1,9 @@
 import { createBrowserRouter,RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
 const router = createBrowserRouter([
-{path:'/',element:<HomePage/>}
+{path:'/',element:<HomePage/>},
+{path:'/products',element:<ProductsPage/>}
 ])
 function App() {
   return <RouterProvider router={router}/>
