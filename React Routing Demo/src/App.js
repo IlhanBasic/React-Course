b1737d66ba7be179b4da97286cc021799ea82143
Ilhan Basic <ilhanbasic456@gmail.com>
2024-09-25 11:37:45 +0200

    Nested Routes

--- React Routing Demo/src/App.js
+++ React Routing Demo/src/App.js
@@ -1,19 +1,31 @@
-import { createBrowserRouter,RouterProvider,createRoutesFromElements, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 // const routerDefinition = createRoutesFromElements(
-  //   <Route>
-  //     <Route path="/" element={<HomePage/>} />
-  //     <Route path="/products" element={<ProductsPage/>} />
-  //   </Route>
-  // );
-  // const router = createBrowserRouter(routerDefinition);
-  const router = createBrowserRouter([
-  {path:'/',element:<HomePage/>},
-  {path:'/products',element:<ProductsPage/>}
-  ])
-  function App() {
-    return <RouterProvider router={router}/>
+//   <Route>
+//     <Route path="/" element={<HomePage/>} />
+//     <Route path="/products" element={<ProductsPage/>} />
+//   </Route>
+// );
+// const router = createBrowserRouter(routerDefinition);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+]);
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
