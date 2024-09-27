8f6edd48c0d76fda9562d514a0bcc3ff8a7dce0f
Ilhan Basic <ilhanbasic456@gmail.com>
2024-09-27 09:54:47 +0200

    Error Handling with Custom Errors

--- Advance React Routing/frontend/src/App.js
+++ Advance React Routing/frontend/src/App.js
@@ -1,15 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
-import EventsPage from "./pages/Events";
+import EventsPage,{loader as eventsLoader} from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventLayout from "./pages/EventRoot";
+import ErrorPage from './pages/Error';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement:<ErrorPage/>,
     children: [
       { index: true, element: <HomePage /> },
       {
@@ -19,15 +21,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch("http://localhost:8080/events");
-
-              if (!response.ok) {
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            },
+            loader: eventsLoader
           },
           { path: ":eventsId", element: <EventDetailPage /> },
           { path: ":eventsId/edit", element: <EditEventPage /> },
--- Advance React Routing/frontend/src/pages/Events.js
+++ Advance React Routing/frontend/src/pages/Events.js
@@ -2,12 +2,24 @@ import {useLoaderData} from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
   return (
     <>
+      {/* {data.isError && <p>{data.message}</p>} */}
       <EventsList events={events} />
     </>
   );
 }
 
 export default EventsPage;
+export async function loader(){
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return {isError:true,message:'Fetch Failed'}
+    throw new Response(JSON.stringify({message:"Couldn't fetch events"}),{status:500})
+  } else {
+    return response;
+  }
+}
