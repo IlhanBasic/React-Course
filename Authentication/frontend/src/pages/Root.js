936adb9a5faea3182246df1e9a08491796d37086
Ilhan Basic <ilhanbasic456@gmail.com>
2025-02-19 08:16:31 +0100

    started with next.js framework

--- Authentication/frontend/src/pages/Root.js
+++ Authentication/frontend/src/pages/Root.js
@@ -1,20 +1,31 @@
-import { Outlet, useLoaderData, useNavigation, useSubmit } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
-import { useEffect } from 'react';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
-  // const navigation = useNavigation();
   const token = useLoaderData();
   const submit = useSubmit();
-  useEffect(()=>{
-    if(!token){
+  // const navigation = useNavigation();
+  useEffect(() => {
+    if (!token) {
       return;
-    }  
-    setTimeout(()=>{
-      submit(null,{action:'/logout',method:'post'})
-    },1*60*60*1000)
-  },[token,submit])
+    }
+
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'post' });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' });
+    }, tokenDuration);
+  }, [token, submit]);
+
   return (
     <>
       <MainNavigation />
