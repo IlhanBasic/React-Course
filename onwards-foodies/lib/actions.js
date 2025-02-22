a32afb777b35acbe487f564febae3da687640f15
Ilhan Basic <ilhanbasic456@gmail.com>
2025-02-22 10:56:18 +0100

    uploading image localy

--- onwards-foodies/lib/actions.js
+++ onwards-foodies/lib/actions.js
@@ -1,13 +1,20 @@
-'use server'
-export async function shareMeal(formData) {
-    const meal = {
-      title: formData.get('title'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email')
-    }
 
-    console.log(meal);
-  }
+'use server';
+
+import { redirect } from 'next/navigation';
+
+import { saveMeal } from './meals';
+
+export async function shareMeal(formData) {
+  const meal = {
+    title: formData.get('title'),
+    summary: formData.get('summary'),
+    instructions: formData.get('instructions'),
+    image: formData.get('image'),
+    creator: formData.get('name'),
+    creator_email: formData.get('email'),
+  };
+
+  await saveMeal(meal);
+  redirect('/meals');
+}
